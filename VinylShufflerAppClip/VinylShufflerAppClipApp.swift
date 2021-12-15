f49178af67a765283e62e965cf95fb623e76a9ad
matt <matt@thinkhappymedium.com>
2021-12-15 17:38:48 -0500

    Added empty app clip target

---
+++ VinylShufflerAppClip/VinylShufflerAppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinylShufflerAppClipApp.swift
+//  VinylShufflerAppClip
+//
+//  Created by Matt Gannon on 12/15/21.
+//
+
+import SwiftUI
+
+@main
+struct VinylShufflerAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
