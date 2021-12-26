ce25abc2861d1a2a8135e152650792952b234cf8
Matt Gannon <gannonm3211@gmail.com>
2021-12-26 14:50:48 -0500

    updated log in log out tests

--- DSSwiftUITests/AppViewModelTests.swift
+++ DSSwiftUITests/AppViewModelTests.swift
@@ -11,6 +11,8 @@ import XCTest
 class AppViewModelTests: XCTestCase {
     
     override func setUpWithError() throws {
+        try super.setUpWithError()
+        
         // Make sure we have a logged out state to start each test
         KeychainManager.shared.remove(key: .discogsUserToken)
         KeychainManager.shared.remove(key: .lastFmSessionKey)
@@ -18,24 +20,43 @@ class AppViewModelTests: XCTestCase {
         XCTAssertNil(KeychainManager.shared.get(for: .lastFmSessionKey))
     }
 
-    func testLogInLogOut() throws {
+    func testLogIn() throws {
+        let viewModel = AppViewModel()
+        
+        KeychainManager.shared.save(key: .discogsUserToken, string: "123")
+        KeychainManager.shared.save(key: .lastFmSessionKey, string: "456")
+
+        let expectation = expectation(description: "Wait for view model to update")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            XCTAssertEqual(viewModel.discogsToken, "123")
+            XCTAssertEqual(viewModel.lastFmKey, "456")
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 0.6)
+
+    }
+    
+    func testLogOut() throws {
         
         // Save fake keys to make sure AppViewModel is getting keys properly on init
         KeychainManager.shared.save(key: .discogsUserToken, string: "123")
-        KeychainManager.shared.save(key: .lastFmSessionKey, string: "123")
-        XCTAssertNotNil(KeychainManager.shared.get(for: .discogsUserToken))
-        XCTAssertNotNil(KeychainManager.shared.get(for: .lastFmSessionKey))
+        KeychainManager.shared.save(key: .lastFmSessionKey, string: "456")
 
         let viewModel = AppViewModel()
         XCTAssertNotNil(viewModel.discogsToken)
         XCTAssertNotNil(viewModel.lastFmKey)
 
         viewModel.logOutAll()
-        XCTAssertNil(KeychainManager.shared.get(for: .discogsUserToken))
-        XCTAssertNil(KeychainManager.shared.get(for: .lastFmSessionKey))
-        XCTAssertNil(viewModel.discogsToken)
-        XCTAssertNil(viewModel.lastFmKey)
+        
+        let expectation = expectation(description: "Wait for vm to update")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            XCTAssertNil(viewModel.discogsToken)
+            XCTAssertNil(viewModel.lastFmKey)
+            expectation.fulfill()
+        }
 
+        wait(for: [expectation], timeout: 0.6)
     }
     
     func testLogOutLastFm() throws {
